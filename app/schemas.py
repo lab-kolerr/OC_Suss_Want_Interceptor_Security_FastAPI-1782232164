b0b4f94176172c8b1be0c30d704954451eef6f37
lab-kolerr <lab.kolerr@kolerr.com>
2026-06-23 23:30:20 +0700

    Initial commit - app/schemas.py

---
+++ app/schemas.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel, EmailStr, validator
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+    @validator('password')
+    def password_strength(cls, v):
+        if len(v) < 8:
+            raise ValueError('Password must be at least 8 characters')
+        return v
